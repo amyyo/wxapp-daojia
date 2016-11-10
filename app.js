a2d603d6ae4feb3f431b697b2e02a673142eaf41
Ivan <wcy5211678@163.com>
2016-11-10 18:32:05 +0800

    framework wxapp

---
+++ app.js
@@ -0,0 +1,40 @@
+// nothing to do
+//
+App({
+  onLaunch() {
+      // App Launch
+      console.log("小程序初始化");
+
+      wx.getLocation({
+      type: ' gcj02', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
+      success: function(res){
+        // success
+        // latitude : 39.90403 longitude : 116.407526
+        console.log(res)
+        var latitude = res.latitude
+        var longitude = res.longitude
+        wx.openLocation({
+          latitude: latitude,
+          longitude: longitude,
+          scale: 28
+        })
+      },
+      fail: function() {
+        // fail
+      },
+      complete: function() {
+        // complete
+      }
+      })
+  },
+
+  onShow() {
+    // App show
+    console.log("小程序显示");
+  },
+
+  onHide() {
+    // App hide
+    console.log("小程序隐藏");
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,5 @@
+Page({
+  onLoad() {
+    console.log("首页加载完成");
+  }
+})
