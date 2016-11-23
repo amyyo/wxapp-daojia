5d9dc51666d92715de2988253beba54ce76e7c50
Ivan <wcy5211678@163.com>
2016-11-24 06:40:40 +0800

    getUserinfo

--- app.js
+++ app.js
@@ -26,6 +26,19 @@ App({
         // complete
       }
     })
+
+    wx.getUserInfo({
+      success: function(res) {
+        console.log(res);
+        var userInfo = res.userInfo
+        var nickName = userInfo.nickName
+        var avatarUrl = userInfo.avatarUrl
+        var gender = userInfo.gender //性别 0：未知、1：男、2：女
+        var province = userInfo.province
+        var city = userInfo.city
+        var country = userInfo.country
+      }
+    })
   },
 
   onShow() {
